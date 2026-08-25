5498f90bbd0f85288aee6653198de3249d8d08b7
Nyisulya <felicianjoseph575@gmail.com>
2026-08-26 00:20:04 +0300

    Seed 76 official members with verified Tanzanian phone numbers into database migration

---
+++ kanda/migrations/0009_seed_official_members.py
@@ -0,0 +1,34 @@
+# Generated Data Migration for Seeding 76 Official Members
+from django.db import migrations
+
+MEMBERS = [('Grace Hosea', '255754070320', 'Sinza & Kijitonyama'), ('Isaac Yohana', '255698246614', 'Sinza & Kijitonyama'), ('Joackim Amos', '255748882603', 'Sinza & Kijitonyama'), ('Jackson Abeid', '255745628916', 'Sinza & Kijitonyama'), ('Janeth Harun', '255614434917', 'Sinza & Kijitonyama'), ('Joan Frank', '255611417740', 'Sinza & Kijitonyama'), ('Joseph Mbote', '255713817570', 'Sinza & Kijitonyama'), ('Joseph R. Temu', '255774724188', 'Sinza & Kijitonyama'), ('Kayumba Bwire', '255683229575', 'Sinza & Kijitonyama'), ('Maandalio James', '255756623779', 'Sinza & Kijitonyama'), ('Elizabeth Mwtia', '255789483731', 'Sinza & Kijitonyama'), ('Happyness Gesasi', '255719040616', 'Sinza & Kijitonyama'), ('Lona Joseph', '255783944027', 'Sinza & Kijitonyama'), ('Agnes Maira', '255754252549', 'Sinza & Kijitonyama'), ('Maria Mwendowa', '255788128192', 'Sinza & Kijitonyama'), ('Mariam Ezekieli', '255652499246', 'Sinza & Kijitonyama'), ('Marygorith Masatu', '255755517985', 'Sinza & Kijitonyama'), ('Masaba Phares', '255687757616', 'Sinza & Kijitonyama'), ('Pendo Charles', '255782486558', 'Sinza & Kijitonyama'), ('Masaka Alex', '255712075190', 'Sinza & Kijitonyama'), ('Michael John', '255713399319', 'Sinza & Kijitonyama'), ('Miriam Mbaga', '255717277830', 'Sinza & Kijitonyama'), ('Monica Mbonguna', '255786148482', 'Sinza & Kijitonyama'), ('Moses David', '255742152959', 'Sinza & Kijitonyama'), ('Moses Mbiligi', '255715230700', 'Sinza & Kijitonyama'), ('Haikael Kiranu', '255782358392', 'Sinza & Kijitonyama'), ('Miriamu Salugole', '', 'Sinza & Kijitonyama'), ('Ester Nyawazwa', '255785641718', 'Sinza & Kijitonyama'), ('Andrew Masawe', '255715179045', 'Sinza & Kijitonyama'), ('Agnes Mwagile', '255769489206', 'Sinza & Kijitonyama'), ('Alen Simon', '255687973249', 'Sinza & Kijitonyama'), ('Amani Semu', '255678274664', 'Sinza & Kijitonyama'), ('Amos Nyangani', '255611419658', 'Sinza & Kijitonyama'), ('Anna Msangi', '255788583605', 'Sinza & Kijitonyama'), ('Baraka Ngamba', '255621629665', 'Sinza & Kijitonyama'), ('Berdina Nyikhyribe', '255764629669', 'Sinza & Kijitonyama'), ('Betty Muso', '255796389024', 'Sinza & Kijitonyama'), ('Bhoke Rooba', '255763128973', 'Sinza & Kijitonyama'), ('Fredrick Manyasi', '255689153637', 'Sinza & Kijitonyama'), ('Christina Masunga', '255748810777', 'Sinza & Kijitonyama'), ('Dorah Luwingu', '255665999902', 'Sinza & Kijitonyama'), ('Dorice Adel', '255787721981', 'Sinza & Kijitonyama'), ('Elia Kilonzo', '255657953993', 'Sinza & Kijitonyama'), ('Elihudi Kamwambi', '255713631716', 'Sinza & Kijitonyama'), ('Elizabeth Kibalo', '255667261886', 'Sinza & Kijitonyama'), ('Eliamin Peter Msangi', '255699351267', 'Sinza & Kijitonyama'), ('Emmanuel Furahini', '255789282305', 'Sinza & Kijitonyama'), ('Emmanuel Walwa', '255743810861', 'Sinza & Kijitonyama'), ('Erick Bamporick', '255766316767', 'Sinza & Kijitonyama'), ('Erick Baneth', '255674669183', 'Sinza & Kijitonyama'), ('Frida George', '255710993527', 'Sinza & Kijitonyama'), ('Frida Mushi', '255652542480', 'Sinza & Kijitonyama'), ('Gasio Masawe', '255713566169', 'Sinza & Kijitonyama'), ('Geoffrey James', '255703451115', 'Sinza & Kijitonyama'), ('Mtaya Joseph', '255666278759', 'Sinza & Kijitonyama'), ('Lukius Muso', '255789545487', 'Sinza & Kijitonyama'), ('Noberth Nikombolwe', '255760301111', 'Sinza & Kijitonyama'), ('Pili Gitiyanni', '255716221950', 'Sinza & Kijitonyama'), ('Prisca Mohamed', '255782273223', 'Sinza & Kijitonyama'), ('Ombeni Musa', '255786622898', 'Sinza & Kijitonyama'), ('Ripoti Ombeni', '255716504804', 'Sinza & Kijitonyama'), ('Raby Hume', '255769241355', 'Sinza & Kijitonyama'), ('Peace Faraja Giwila', '255750773537', 'Sinza & Kijitonyama'), ('Silvester Joseph', '255618689300', 'Sinza & Kijitonyama'), ('Steven Joseph', '255677346764', 'Sinza & Kijitonyama'), ('Tawa Tenga', '255755000491', 'Sinza & Kijitonyama'), ('Tegra Komba', '255699501922', 'Sinza & Kijitonyama'), ('Teresia Mambo', '255718755833', 'Sinza & Kijitonyama'), ('William Eliya', '255765460310', 'Sinza & Kijitonyama'), ('Janeth Kapalu', '255716106770', 'Sinza & Kijitonyama'), ('Stepano Anania', '255749942885', 'Sinza & Kijitonyama'), ('Lenny Michael', '255715198498', 'Sinza & Kijitonyama'), ('Revocatus Masalu', '255658082866', 'Sinza & Kijitonyama'), ('Sarah Charles', '255714304140', 'Sinza & Kijitonyama'), ('Aron Mtema', '255683637884', 'Sinza & Kijitonyama'), ('Ibrahim Paul', '255719561596', 'Sinza & Kijitonyama')]
+
+def seed_members(apps, schema_editor):
+    Mshiriki = apps.get_model('kanda', 'Mshiriki')
+    for name, phone, family in MEMBERS:
+        if phone:
+            mshiriki, created = Mshiriki.objects.get_or_create(
+                simu=phone,
+                defaults={'jina': name, 'familia': family, 'jukumu': 'MSHIRIKI', 'is_active': True}
+            )
+            if not created:
+                mshiriki.jina = name
+                mshiriki.is_active = True
+                mshiriki.save()
+        else:
+            # Bila simu
+            if not Mshiriki.objects.filter(jina=name).exists():
+                Mshiriki.objects.create(jina=name, simu='', familia=family, jukumu='MSHIRIKI', is_active=True)
+
+def unseed_members(apps, schema_editor):
+    pass
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('kanda', '0008_alter_mshiriki_picha'),
+    ]
+
+    operations = [
+        migrations.RunPython(seed_members, reverse_code=unseed_members),
+    ]
